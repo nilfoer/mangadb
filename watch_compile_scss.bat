537004c1b99e77c95104e6e92920e112411a8602
nilfoer <60221153+nilfoer@users.noreply.github.com>
2020-10-20 18:03:47 +0200

    fixed unequallay sized grid items when the row isn't filled re-adjusted media queries

--- watch_compile_scss.bat
+++ watch_compile_scss.bat
@@ -1,1 +1,1 @@
-..\tools\dart-sass\sass.bat --watch --no-source-map manga_db/webGUI/sass/style.scss:manga_db/webGUI/static/style.css 
+..\tools\dart-sass\sass.bat --watch --style=compressed --no-source-map manga_db/webGUI/sass/style.scss:manga_db/webGUI/static/style.css 
