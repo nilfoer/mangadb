648d5b4a4a6163ef52b5fa8257e77fac4fb69973
nilfoer <60221153+nilfoer@users.noreply.github.com>
2021-01-26 05:13:01 +0100

    bump version

--- manga_db/__init__.py
+++ manga_db/__init__.py
@@ -1,5 +1,5 @@
 from .logging_setup import configure_logging
 
-VERSION = '0.21.0'
+VERSION = '0.21.1'
 
 configure_logging("manga_db.log")
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
     name="MangaDB",
-    version="0.21.0",
+    version="0.21.1",
     description="Organize your manga reading habits",
     long_description=long_description,
     long_description_content_type="text/markdown",
